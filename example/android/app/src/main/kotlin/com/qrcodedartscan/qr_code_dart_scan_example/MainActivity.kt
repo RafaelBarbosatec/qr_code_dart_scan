d396de8eabf6c9a8535ed3803349fb53dda4f22c
rafaelalmeidacloudwalk <rafael.almeida@cloudwalk.io>
2026-07-28 10:52:37 -0300

    chore: update dependencies and Android project structure
    - Updated camera and related packages to their latest versions in pubspec.yaml and pubspec.lock.
    - Migrated Android project configuration from Groovy to Kotlin DSL.
    - Enhanced gradle properties for improved performance.
    - Cleaned up AndroidManifest.xml and MainActivity.kt for better readability.
    - Updated Dart SDK and Flutter version constraints in pubspec.lock.

--- example/android/app/src/main/kotlin/com/qrcodedartscan/qr_code_dart_scan_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/qrcodedartscan/qr_code_dart_scan_example/MainActivity.kt
@@ -2,4 +2,4 @@ package com.qrcodedartscan.qr_code_dart_scan_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
